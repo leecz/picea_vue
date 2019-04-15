fed98be5477c94969bf09544f8f4c2164cd4cae5
lichengzhi <chengzhilee2@gmail.com>
2019-04-15 15:24:21 +0800

    update base url config

--- src/common/config.js
+++ src/common/config.js
@@ -1,3 +1,6 @@
 // export const API_URL = "https://conduit.productionready.io/api";
-export const API_URL = "http://localhost:4000";
-export default API_URL;
+let API_URL = "http://localhost:4000";
+if (process.env.NODE_ENV === "production") {
+  API_URL = "http://192.168.2.72:4000";
+}
+export { API_URL };
