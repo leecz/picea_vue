dc0a77a3df9d94c56290e8ae71c2ab9e700ae76a
lichengzhi <chengzhilee2@gmail.com>
2019-04-16 10:55:49 +0800

    update api url config

--- src/common/config.js
+++ src/common/config.js
@@ -1,6 +1,3 @@
 // export const API_URL = "https://conduit.productionready.io/api";
-let API_URL = "http://localhost:4000";
-if (process.env.NODE_ENV === "production") {
-  API_URL = "http://192.168.2.72:4000";
-}
-export { API_URL };
+export const API_URL = process.env.VUE_APP_API_URL;
+export default API_URL;
