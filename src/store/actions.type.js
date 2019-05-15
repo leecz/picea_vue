8884582e9856ea4d4236a2a0780fd57687f8eca0
lichengzhi <chengzhilee2@gmail.com>
2019-05-15 15:31:24 +0800

    add fixed column config to echart

--- src/store/actions.type.js
+++ src/store/actions.type.js
@@ -5,3 +5,4 @@ export const REGISTER = "register";
 export const UPDATE_USER = "updateUser";
 export const CREATE_CHART = "createChart";
 export const UPDATE_CHART = "updateChart";
+export const SET_CHART_SERIES = "setChartSeries";
