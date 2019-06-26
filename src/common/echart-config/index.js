edc9f927ded9d742f659726aa9b612dadf459eb5
lichengzhi <chengzhilee2@gmail.com>
2019-06-26 15:30:48 +0800

    update data tip way

--- src/common/echart-config/index.js
+++ src/common/echart-config/index.js
@@ -35,23 +35,27 @@ export default {
     fixed_column: false,
     multi_series: false,
     orthogonal_coordinates: false,
-    data_type: "json"
+    data_type: "json",
+    data_desc: "根据json结构填写数据"
   },
   graph: {
     fixed_column: false,
     multi_series: false,
     orthogonal_coordinates: false,
-    data_type: "json"
+    data_type: "json",
+    data_desc: "根据json结构填写数据"
   },
   radar: {
     fixed_column: true,
     multi_series: false,
     orthogonal_coordinates: false,
-    data_type: "json"
+    data_type: "json",
+    data_desc: "根据json结构填写数据"
   },
   liquidFill: {
     fixed_column: false,
     orthogonal_coordinates: false,
-    data_type: "json"
+    data_type: "json",
+    data_desc: "根据json结构填写数据"
   }
 };
